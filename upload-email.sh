c46f9679dbfd36cb6079ab0af9a7764e4a0fe03c
Aleksander Lenart <aleksander@lenart.pl>
2025-01-08 21:36:50 +0100

    Add individual files, multiple emails

--- upload-email.sh
+++ upload-email.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+
 cd '/volume1/Assets/Uploads' || exit 
-ls > current_dir 
-current_dir_len=$(wc -l < current_dir) 
-prior_dir_len=$(wc -l < prior_dir) 
-num_new_items=$((current_dir_len - prior_dir_len)) 
-if ! (diff current_dir prior_dir> /dev/null) && [ "$num_new_items" -gt 0 ] 
-then /usr/bin/php -r "mail('alex@printhouse.co.uk','DataServer Upload Notification','Greetings Master, An additional $num_new_items new item-s have been uploaded to the Upload folder. Have a nice day! Synology');" 
-fi 
+
+# Generate a list of current directory structure including files
+find . -type f > current_dir 
+
+# Find added items
+added_items=$(comm -13 prior_dir current_dir)
+
+# Prepare email body if there are new files
+if [ -n "$added_items" ]; then
+    num_new_items=$(echo "$added_items" | wc -l)
+    email_body="Greetings Master,\n\nAn additional $num_new_items new item(s) have been uploaded:\n\n$added_items\n\nHave a nice day!\nSynology"
+
+    # Send email notification to multiple recipients
+    /usr/bin/php -r "
+    mail('user1@example.com', 'Server Upload Notification', '$email_body');
+    mail('user2@example.com', 'Server Upload Notification', '$email_body');
+    mail('user3@example.com', 'Server Upload Notification', '$email_body');
+    "
+fi
+
+# Update prior directory snapshot
 cp current_dir prior_dir
